1a100bfda56f34f2cb8b9af4d40b271c91bedaac
kamohelotshabalala <kamohelotshabalala32@gmail.com>
2025-06-18 21:29:27 +0200

    Improved user experience and updated poem classlist

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ function displayPoem(response){
     delay: 1,
     cursor: null,
   });
+ 
 }
 
 
@@ -14,10 +15,12 @@ function generatePoem(event) {
     let poemBoxElement = document.querySelector("#poem");
 let questionInput = document.querySelector("#question-input");
 let apiKey = "46o4ft0aa94fd28cbcf4b985bc283d4f";
-let context ="Your are a smart AI assisant and you know everything. please generate poems and separate each line with a <br /> and space after each line. Do not write a line on top of the other. Make sure to follow User Instructions. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem.";
+let context ="Your are a smart AI assisant and you know everything. please generate a 6 line poem, and separate each line with a <br/> and space after each line. Do not write a line on top of the other. Make sure to follow User Instructions. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem.";
 let prompt = `User Instructions: Generate a poem about  ${questionInput.value}`;
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
+let poem = document.querySelector("#poem");
+poem.classList.remove("hidden");
+poem.innerHTML = `<div class="blink">⌛ Generating something for you about ${questionInput.value}, please wait...😊 </div>`
 
 axios.get(apiUrl).then(displayPoem);
     
