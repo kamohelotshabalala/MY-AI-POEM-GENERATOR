8f2e5a861e98cedb20b6b329c72a040e9418dec1
kamohelotshabalala <kamohelotshabalala32@gmail.com>
2025-06-18 14:53:43 +0200

    Created new  display function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+function displayPoem(response){
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: null,
+  });
+}
+
+
+
 function generatePoem(event) {
     event.preventDefault();
     let poemBoxElement = document.querySelector("#poem");
-    new Typewriter("#poem", {
-      strings: "poem",
-      autoStart: true,
-      delay: 1,
-      cursor: null,
-    });
+let questionInput = document.querySelector("#question-input");
+let apiKey = "46o4ft0aa94fd28cbcf4b985bc283d4f";
+let context ="Your are a smart AI assisant and you know everything, please generate a 4 line interesting poems and separate each line with a <br /> and space after each line. Do not write a line on top of the other. Make sure to follow User Instructions. Sign the poem with 'SheCodes AI inside a <strong> element at the end of the poem.";
+let prompt = `User Instructions: Generate a poem about  ${questionInput.value}`;
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+
+axios.get(apiUrl).then(displayPoem);
+    
 }
 
 let poemElement = document.querySelector("#form-generator");
