0cf6ca4ecda14609909dc931b4079f699f9bfaa1
kamohelotshabalala <kamohelotshabalala32@gmail.com>
2025-06-19 12:39:17 +0200

    CSS updated line height of poem class

--- src/index.js
+++ src/index.js
@@ -8,14 +8,12 @@ function displayPoem(response){
  
 }
 
-
-
 function generatePoem(event) {
     event.preventDefault();
     let poemBoxElement = document.querySelector("#poem");
 let questionInput = document.querySelector("#question-input");
 let apiKey = "46o4ft0aa94fd28cbcf4b985bc283d4f";
-let context ="Your are a smart AI assisant and you know everything. please generate a 6 line poem, and separate each line with a <br/> and space after each line. Do not write a line on top of the other. Make sure to follow User Instructions. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem.";
+let context ="Your are a smart AI assisant and you know everything. please generate a 6 line poem and separate each line with a <br/> and space after each line. Do not write a line on top of the other. Make sure to follow User Instructions. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem.";
 let prompt = `User Instructions: Generate a poem about  ${questionInput.value}`;
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 let poem = document.querySelector("#poem");
